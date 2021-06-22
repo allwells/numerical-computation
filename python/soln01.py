e8b953a1727a54043a3e92022babefdb08991532
allwells <aleenfestus@gmail.com>
2021-06-22 16:37:59 +0100

    Update /python/soln01.py

--- python/soln01.py
+++ python/soln01.py
@@ -2,6 +2,26 @@ import math
 
 h = 0.2
 a = 0.0
+b = math.pi / 2
 
-def f(x):
-    pass
+def f(value):
+    result = value  - math.cos(value)
+    return round(result, 4)
+
+def c(a, b):
+    result = (a + b) / 2
+    return round(result, 4)
+
+
+while (b - a) > h:
+    c = c(a, b)
+    print('C =', round(c, 4))
+
+    if (f(b) * f(a)) > 0:
+        a = c
+        print('New A:', round(a, 4))
+    else:
+        b = c
+        print('New B:', round(b, 4))
+    
+    print('b - a =', round((a - b), 4), "\n\n")
