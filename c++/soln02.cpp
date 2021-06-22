6d63f2683211a1d37b689f5c34ae83353d2db5b9
allwells <aleenfestus@gmail.com>
2021-06-23 00:00:11 +0100

    Update C/C++

--- c++/soln02.cpp
+++ c++/soln02.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main() {
-    cout << "Hello Ella";
+    // Formula: Xn+1 = Xn + C/Xn
+    float X0 = 2;
+    float C = 5;
+    cout << "Formula: Xn+1 = Xn + C/Xn\n";
+    cout << "X0: %d\n", X0;
 
+    for (int n = 1; n < 12; n++) {
+        float Xn = (X0 + (C / X0)) / 2;
+        if (Xn == X0) {
+            break;
+        } else {
+            cout << "X", n, ": \n", Xn;
+            X0 = Xn;
+        }
+    }
     return 0;
 }
--- c/soln02.c
+++ c/soln02.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello Ella");
+    // Formula: Xn+1 = Xn + C/Xn
+    float X0 = 2;
+    float C = 5;
+    printf("Formula: Xn+1 = Xn + C/Xn\n");
+    printf("X0: %f\n", X0);
+
+    for (int n = 1; n < 12; n++) {
+        float Xn = (X0 + (C / X0)) / 2;
+        if (Xn == X0) {
+            break;
+        } else {
+            printf("X%d", n, ": %9.6f\n", Xn);
+            X0 = Xn;
+        }
+    }
 
     return 0;
 }
